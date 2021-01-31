aae681e75c07200707c3de9419fec5d1faf27cfe
Ala2Hamdan <ala2hamdan92@gmail.com>
2021-01-31 15:27:45 +0200

    add function

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ var nameUser =prompt('enter your nmae please');
 document.write("<h3> welcome</h3> "+nameUser+" <h3> to clothing store</h3>");
 
 var num =prompt('enter servie number you want that esist before the list services (1-Free Delivery...2-Return the product..3-Discount)');  
-
+function Service1(){
 while(! (num >0 && num < 4) ){
 
      num =prompt('enter correct servie number ');
 }
+}
+Service1();
 
-
+function srevice2(){
 if (num == 1 ){
 
   alert('provide up with your iformation');
@@ -25,16 +27,20 @@ if (num == 1 ){
 {
     var x = confirm("try again");
 }
-var e =prompt('how you satisfiy about our services , evaluate us from 1 to 5');
+}
+srevice2();
 
 
+var e =prompt('how you satisfied about our services , evaluate us from 1 to 5');
 
+
+function satisfy (){
 for (var i = 0 ; i < e; i++){
 
     document.write('<img src="https://image.shutterstock.com/z/stock-vector-vector-free-delivery-free-shipping-hour-and-fast-delivery-icons-set-157317164.jpg" width="100px" height="100px">')
 
 }
-
-
+}
+satisfy ();
 
 
